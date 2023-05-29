35ece4b9416abf0ef5bc7003788cba8e5f32550b
xlivix <oliviabryanttt@gmail.com>
2023-05-29 14:35:09 -0400

    leaflet-challenge

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -17,24 +17,23 @@ function getMarkerSize(magnitude) {
 
 // Function to determine the marker color based on depth
 function getMarkerColor(depth) {
-    var colors = ["#00ff99", "#33ff00", "#ccff00", "#ffcc00", "#ff6600", "#ff0000"];
-    if (depth < 10) {
-        return colors[0]; // Green
+    if (depth >= -10 && depth <= 10) {
+        return '#00FF00'; // green-lime color square
     }
-    else if (depth < 30) {
-        return colors[1]; // Lime
+    else if (depth > 10 && depth <= 30) {
+        return '#ADFF2F'; // yellow-green color square
     }
-    else if (depth < 50) {
-        return colors[2]; // Yellow
+    else if (depth > 30 && depth <= 50) {
+        return '#FFFF00'; // yellow-orange color square
     }
-    else if (depth < 70) {
-        return colors[3]; // Orange
+    else if (depth > 50 && depth <= 70) {
+        return '#FFA500'; // orange color square
     }
-    else if (depth < 90) {
-        return colors[4]; // Red-Orange
+    else if (depth > 70 && depth <= 90) {
+        return '#FF4500'; // red-orange color square
     }
     else {
-        return colors[5]; // Red
+        return '#FF0000'; // red color square
     }
 }
 
@@ -48,82 +47,33 @@ d3.json(url).then(function(data) {
                 fillOpacity: 0.7,
                 color: "#000",
                 weight: 0.5
-            }).bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2] + "</p>");
+            //}).bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2] + "</p>");
+            }).bindPopup(`<strong>Magnitude:</strong> ${feature.properties.mag}<br><strong>Location:</strong> ${feature.properties.place}<br><strong>Depth:</strong> ${feature.geometry.coordinates[2]} km`);
         }
     }).addTo(myMap);
-    // Legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-        var div = L.DomUtil.create("div", "info legend");
-        var depths = [-10, 10, 30, 50, 70, 90];
-        var labels = [];
-        for (var i = 0; i < depths.length; i++) {
-            div.innerHTML += '<i class="legend-dot" style="background:' + getMarkerColor(depths[i] + 1) + '"></i> ' + depths[i] + (depths[i + 1] ? "&ndash;" + depths[i + 1] + "<br>" : "+");
+
+    // Create the legend control
+    const legend = L.control({ position: 'bottomright' });
+
+    // Function to generate the HTML for the legend
+    legend.onAdd = function (map) {
+        const div = L.DomUtil.create('div', 'legend');
+        const colors = [
+            { color: '#00FF00', range: '-10 - 10' },
+            { color: '#ADFF2F', range: '10 - 30' },
+            { color: '#FFFF00', range: '30 - 50' },
+            { color: '#FFA500', range: '50 - 70' },
+            { color: '#FF4500', range: '70 - 90' },
+            { color: '#FF0000', range: '90+' }
+        ];
+        let labels = '';
+        for (let i = 0; i < colors.length; i++) {
+            labels += '<div class="legend-item">' + '<div class="legend-color" style="background:' + colors[i].color + '"></div>' + '<div class="legend-range">' + colors[i].range + '</div>' + '</div>';
         }
+        div.innerHTML = labels;
         return div;
     };
+
+    // Add the legend to the map
     legend.addTo(myMap);
 });
-
-/*
-// Create the map
-const map = L.map('map').setView([0, 0], 2);
-
-// Add the tile layer to the map
-const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-}).addTo(map);
-
-// Fetch the earthquake data
-const earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-fetch(earthquakeUrl).then(response => response.json()).then(earthquakeData => {
-    // Create an earthquakes layer group
-    const earthquakes = L.layerGroup();
-    // Loop through the earthquake data
-    earthquakeData.features.forEach(feature => {
-        const earthquake = feature.properties;
-        // Get the latitude, longitude, magnitude, and depth of the earthquake
-        const lat = feature.geometry.coordinates[1];
-        const lng = feature.geometry.coordinates[0];
-        const magnitude = earthquake.mag;
-        const depth = feature.geometry.coordinates[2];
-        // Define the marker size and color based on magnitude and depth
-        const markerSize = magnitude * 5;
-        const markerColor = getColor(depth);
-        // Create a marker with tooltip
-        const marker = L.circleMarker([lat, lng], {
-            radius: markerSize,
-            fillColor: markerColor,
-            color: '#000',
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-        }).bindTooltip(`<strong>Magnitude:</strong> ${magnitude}<br><strong>Location:</strong> ${earthquake.place}<br><strong>Depth:</strong> ${depth} km`);
-        // Add the marker to the earthquakes layer group
-        earthquakes.addLayer(marker);
-    });
-    // Add the earthquakes layer to the map
-    earthquakes.addTo(map);
-    // Fit the map bounds to the earthquakes layer
-    map.fitBounds(earthquakes.getBounds());
-    // Function to get the marker color based on depth
-    function getColor(depth) {
-        return depth > 100 ? '#FF0000' : depth > 50  ? '#FFA500' : depth > 10  ? '#FFFF00' : '#00FF00';
-    }
-    // Create a legend control
-    const legend = L.control({ position: 'bottomright' });
-    // Function to generate the legend content
-    legend.onAdd = function () {
-        const div = L.DomUtil.create('div', 'info legend');
-        const depths = [0, 10, 50, 100];
-        const labels = [];
-        for (let i = 0; i < depths.length; i++) {
-            div.innerHTML += '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' + depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
-        }
-        return div;
-    };
-    // Add the legend to the map
-    legend.addTo(map);
-});
-*/
